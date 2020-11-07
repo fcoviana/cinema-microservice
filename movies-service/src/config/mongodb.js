d4f9154eda16ea888a5aaad6d5cd42635f6ed805
Francisco Viana <franciscoviana862@gmail.com>
2020-11-07 10:04:16 -0300

    fix

--- movies-service/src/config/mongodb.js
+++ movies-service/src/config/mongodb.js
@@ -7,13 +7,12 @@ function connect(callback) {
     if (connection) return callback(null, db);
 
     MongoClient.connect(process.env.MONGO_CONNECTION, (err, conn) => {
-        if (err)
-            return callback(err, null);
-        else {
-            connection = conn;
-            db = conn.db(process.env.DATABASE_NAME);
-            return callback(null, db);
-        }
+        if (err) return callback(err, null);
+
+        connection = conn;
+        db = conn.db(process.env.DATABASE_NAME);
+        return callback(null, db);
+
     })
 }
 
--- movies-service/src/repository/repository.js
+++ movies-service/src/repository/repository.js
@@ -28,4 +28,38 @@ function disconnect() {
     return mongodb.disconnect();
 }
 
+function create(callback) {
+    mongodb.connect((err, db) => {
+    
+        db.collection("movies").insert([
+            {
+                titulo: "Os Vingadores: Guerra Infinita",
+                sinopse: "Os heróis mais poderosos da Marvel enfrentando o Thanos",
+                duracao: 120,
+                dataLancamento: ISODate("2018-11-01T00:00:00Z"),
+                imagem: "https://www.jornalcontabil.com.br/wp-content/uploads/2019/04/VINGADORES.jpg",
+                categorias: ["Aventura", "Ação"]
+            },
+            {
+                titulo: "Os Vingadores: Era de Ultron",
+                sinopse: "Os heróis mais poderosos da Marvel enfrentando o Ultron",
+                duracao: 110,
+                dataLancamento: ISODate("2016-11-01T00:00:00Z"),
+                imagem: "https://www.jornalcontabil.com.br/wp-content/uploads/2019/04/VINGADORES.jpg",
+                categorias: ["Aventura", "Ação"]
+            },
+            {
+                titulo: "Os Vingadores",
+                sinopse: "Os heróis mais poderosos da Marvel enfrentando o Loki",
+                duracao: 100,
+                dataLancamento: ISODate("2014-11-01T00:00:00Z"),
+                imagem: "https://www.jornalcontabil.com.br/wp-content/uploads/2019/04/VINGADORES.jpg",
+                categorias: ["Aventura", "Ação"]
+            }
+        ], callback);
+    });
+}
+
+// create((err, movies) => console.log('wewew'))
+
 module.exports = { getAllMovies, getMovieById, getMoviePremiers, disconnect }
