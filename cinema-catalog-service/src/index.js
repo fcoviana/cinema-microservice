0f14df075ef096fb039188460d555e77a2d19b8b
Francisco Viana <franciscoviana862@gmail.com>
2020-11-08 12:33:30 -0300

    index

--- cinema-catalog-service/src/index.js
+++ cinema-catalog-service/src/index.js
@@ -1,7 +1,8 @@
 require("dotenv-safe").config();
+const cinemaCatalog = require('./api/cinema-catalog');
 const server = require("./server/server");
 const repository = require("./repository/repository");
  
-server.start(movies, repository, (err, app) => { 
+server.start(cinemaCatalog, repository, (err, app) => { 
     console.log("just started");
 });
